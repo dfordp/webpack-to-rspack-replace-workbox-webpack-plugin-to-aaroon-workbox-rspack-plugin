3f00310239fd2b941c42ff35c7f63a4e66647a1a
Dilpreet <92905896+dfordp@users.noreply.github.com>
2024-10-18 00:09:53 +0530

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,34 @@ export default function transform(file, api, options) {
   const root = j(file.source);
   let dirtyFlag = false;
 
-  // Find require statements
+  // Transform CommonJS require statements
   root.find(j.CallExpression, {
-    callee: { name: 'require' },
-    arguments: [{ value: 'workbox-webpack-plugin' }]
-  }).forEach(path => {
-    // Update the require argument to the new package
-    path.node.arguments[0].value = '@aaroon/workbox-rspack-plugin';
-    dirtyFlag = true;
+      callee: { name: 'require' },
+      arguments: [{ value: 'workbox-webpack-plugin' }],
+  }).forEach((path) => {
+      if (
+          j.Literal.check(path.node.arguments[0]) &&
+          path.node.arguments[0].value === 'workbox-webpack-plugin'
+      ) {
+          path.node.arguments[0] = j.literal('@aaroon/workbox-rspack-plugin');
+          dirtyFlag = true;
+      }
+  });
+
+  // Transform ES6 import statements
+  root.find(j.ImportDeclaration, {
+      source: { value: 'workbox-webpack-plugin' },
+  }).forEach((path) => {
+      if (
+          j.Literal.check(path.node.source) &&
+          path.node.source.value === 'workbox-webpack-plugin'
+      ) {
+          path.node.source = j.literal('@aaroon/workbox-rspack-plugin');
+          dirtyFlag = true;
+      }
   });
 
   return dirtyFlag ? root.toSource() : undefined;
 }
 
-
-export const parser = "tsx";
+export const parser = 'tsx';
